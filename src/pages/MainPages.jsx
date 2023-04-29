4e72de316a1c44da546a8ed359c7bebff0a4c0a1
victorbosneag <victor.bosneag@hotmail.com>
2023-04-29 18:32:10 +0300

    styling

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from '../components/navbar'
-import LandingPage from './LandingPage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../components/navbar";
+import LandingPage from "./LandingPage";
 
 function MainPages() {
   return (
     <div>
-    <Navbar></Navbar>
-    <Routes>
+      <Navbar></Navbar>
+      <Routes>
         <Route index element={<LandingPage />} />
-    </Routes>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default MainPages
+export default MainPages;
