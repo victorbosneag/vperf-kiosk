2eacb5236594b950deec37d92dced813eeb1ef9c
victorbosneag <victor.bosneag@hotmail.com>
2023-05-09 20:01:38 +0300

    changed logo

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://serverproxy.home.arpa:9443"
 export const IMAGE_URL = "http://localhost:3000/wall.png"
-export const LOGO_URL = "http://localhost:3000/performante_galben.png"
+export const LOGO_URL = "http://localhost:3000/performante_bleu_marin.png"
