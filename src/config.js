72c35bf6a3278fe7bbe658ed06a6f7e92413411c
victorbosneag <victor.bosneag@hotmail.com>
2024-05-15 10:34:00 +0300

    modified configuration to better suit prod env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = "https://serverproxy.home.arpa:9443"
-export const IMAGE_URL = "http://localhost:3000/wall.png"
-export const LOGO_URL = "http://localhost:3000/performante_galben.png"
-export const TESTING_MODE = true
+export const API_URL = "https://172.16.5.144:1031"
+export const IMAGE_URL = "https://172.16.5.144:3000/wall.png"
+export const LOGO_URL = "https://172.16.5.144:3000/performante_galben.png"
+export const TESTING_MODE = false
 export const EFFECT_INTERVAL = 2
